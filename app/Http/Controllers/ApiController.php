6d628534f783e8708792a41ba484a4794df3e53c
Carlos <carlosroldanmontaner@gmail.com>
2021-06-02 20:35:26 +0200

    Adding production URL to swagger

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 /**
 * @OA\Info(title="WeWorkout API", version="1.0", description="Documentación de la API del backend de la aplicación WeWorkout. La mayoría de llamadas utilizan autenticación mediante JWT. Se incluyen ejemplos de request y de response.")
 *
-* @OA\Server(url="http://127.0.0.1:8000")
+* @OA\Server(url="http://127.0.0.1:8000", description="Local server")
+* @OA\Server(url="https://weworkoutbackend.herokuapp.com/", description="Prod server")
 */
 
 /**
